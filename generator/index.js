53b8012b30951e8dd342c4c4a8e981f77cf9d1ef
i5ting <i5ting@126.com>
2017-11-06 20:01:49 +0800

    lazycommmit, please ignore

---
+++ generator/index.js
@@ -0,0 +1,16 @@
+
+// var tpl = require('tpl_apply_art');
+
+
+// var source = process.cwd() + '/tpl.js'
+// var dest = process.cwd() + '/test/tpl.generate.js'
+
+
+// tpl.tpl_apply_art(source, {
+// 	title: "My New Post", body: "This is my first post!"
+// }, dest);
+
+
+module.exports = function (model, dest) {
+
+}
