a25bf8d7be79a377c4c5010954e846e5ad84823a
狼叔 <langshu.ssl@alibaba-inc.com>
2017-11-20 18:50:50 +0800

    lazycommmit, please ignore

--- src/users/router.js
+++ src/users/router.js
@@ -1,10 +1,24 @@
 const router = require('koa-router')()
 
-router.prefix('/users')
+// router.prefix('/users')
 
 router.get('/', function (ctx, next) {
   ctx.render('src/users/index', {
     title: "2323"
+  })
+})
+
+
+router.get('/login', function (ctx, next) {
+  ctx.render('src/users/login', {
+    title: "login"
+  })
+})
+
+
+router.get('/register', function (ctx, next) {
+  ctx.render('src/users/register', {
+    title: "register"
   })
 })
 
